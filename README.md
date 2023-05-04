f4d1d8fdd8fbc297f8dc722516bc81f00c595b59
Melisha Evans <melisha.evans06@gmail.com>
2023-05-04 09:37:49 -0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# password-app
+# Password Application
+
+![password application](./assets/images/pw_image.jpg)
+
+
+<link to the deployed app>
+
+
+
+## Description
+
+This application is to generate a password that is no less than 8 characters long.
+
+This application will help create a password when prompted.
--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 // Assignment Code
+var number = 0;
+var special = 0;
+var capital = 0;
+var characters = 0;
+
+var options = ['0123456789abcdefghijklmnopqrstuvwxyz!"#$%~ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+
 var generateBtn = document.querySelector("#generate");
 
 var password = document.getElementById('password');
 
 
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // At least 8 characters long.
+// Contains at least one number.
+// Contains at least one capital letter.
+// Contains at least one of the following special characters !, ", #, $, %, or ~.
+// validate input
+var passwordText = document.querySelector("#password");
   var chars = '0123456789abcdefghijklmnopqrstuvwxyz!"#$%~ABCDEFGHIJKLMNOPQRSTUVWXYZ'
   var length = 8;
   var password = '';
@@ -18,7 +29,6 @@ function writePassword() {
     var randomNum = Math.floor(Math.random() * chars.length);
     password += chars.substring(randomNum, randomNum +1);
   }
-  
     document.getElementById('password').value = password;
 }
 function generatePassword() {
@@ -28,4 +38,3 @@ function generatePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 generatePassword();
-
